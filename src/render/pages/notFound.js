1e49f11c2a22c0b49f735667d77812db19c2dc58
Douglas Paz <eu@douglaspaz.com>
2026-07-13 10:25:02 -0300

    feat: add 200.html page for not found issue #8

---
+++ src/render/pages/notFound.js
@@ -0,0 +1,10 @@
+import renderBase from '../base.js'
+
+export default () => renderBase({
+  title: 'Guia Prowlarr - Catálogo BeTor',
+  content: `
+    <section>
+      <h1>Nada por aqui...</h1>
+    </section>
+  `
+})
